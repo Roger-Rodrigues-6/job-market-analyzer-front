6a1bf2d2aa834c1d8cac9e70c50909d534b35a04
Roger-Rodrigues-6 <roger6rs@gmail.com>
2026-03-19 18:00:38 -0300

    fix: code clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "vr0omm6j8p");
+            })(window, document, "clarity", "script", "vydu5u17jl");
           `}
         </Script>
 
